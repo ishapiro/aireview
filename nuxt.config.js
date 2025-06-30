1be4f7663c7ab2062b8771ce3ccaac18b949c8af
ishapiro <ishapiro@cogitations.com>
2025-06-30 09:50:53 +0300

    Fixed all categories Added to nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -59,7 +59,8 @@ export default defineNuxtConfig({
         '/terms',
         '/reviews/*',
         '/categories',
-        '/categories/*'
+        '/categories/*',
+        '/categories-all'
       ]
     },
     clientOptions: {
